b56223a42c9c465fdcfcc460ada5776b39f6eb65
李凯荣 <chronnk@qq.com>
2023-06-19 14:49:49 +0800

    finish p1,add docs

--- src/record/rm_scan.cpp
+++ src/record/rm_scan.cpp
@@ -19,6 +19,9 @@ RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
     // Todo:
     // 初始化file_handle和rid（指向第一个存放了记录的位置）
 
+    rid_.page_no = 1;
+    auto page_handle = file_handle_->fetch_page_handle(rid_.page_no);
+    rid_.slot_no = Bitmap::first_bit(1, page_handle.bitmap, page_handle.file_hdr->bitmap_size);
 }
 
 /**
@@ -27,7 +30,17 @@ RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
 void RmScan::next() {
     // Todo:
     // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
+    auto page_handle = file_handle_->fetch_page_handle(rid_.page_no);
 
+    rid_.slot_no =
+        Bitmap::next_bit(true, page_handle.bitmap, file_handle_->file_hdr_.num_records_per_page, rid_.slot_no);
+    if (rid_.slot_no == file_handle_->file_hdr_.num_records_per_page) {
+        rid_.page_no++;
+        if (rid_.page_no < file_handle_->file_hdr_.num_pages) {
+            auto page_handle = file_handle_->fetch_page_handle(rid_.page_no);
+            rid_.slot_no = Bitmap::first_bit(1, page_handle.bitmap, page_handle.file_hdr->bitmap_size);
+        }
+    }
 }
 
 /**
@@ -35,13 +48,10 @@ void RmScan::next() {
  */
 bool RmScan::is_end() const {
     // Todo: 修改返回值
-
-    return false;
+    return (rid_.page_no == file_handle_->file_hdr_.num_pages);
 }
 
 /**
  * @brief RmScan内部存放的rid
  */
-Rid RmScan::rid() const {
-    return rid_;
-}
+Rid RmScan::rid() const { return rid_; }
